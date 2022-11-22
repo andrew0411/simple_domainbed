f3c33b8e7b1258694ee26488080c2e7d7b83f195
Lee, Jaehwan <jaehwanee0411@gmail.com>
2022-11-22 23:28:04 +0900

    Update transforms.py

--- domainbed/datasets/transforms.py
+++ domainbed/datasets/transforms.py
@@ -12,7 +12,7 @@ aug = T.Compose(
     [
         T.RandomResizedCrop(224, scale=(0.7, 1.0)),
         T.RandomHorizontalFlip(),
-        T.ColorJitter(0.3, 0.3, 0.3, 0.3, 0.3),
+        T.ColorJitter(0.3, 0.3, 0.3, 0.3),
         T.RandomGrayscale(p=0.1),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
