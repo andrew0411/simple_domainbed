55e009ba3caaad6b0fab6f03e8651e26a7f85a4e
Lee, Jaehwan <jaehwanee0411@gmail.com>
2022-11-29 21:25:59 +0900

    Create train_all.py

---
+++ train_all.py
@@ -0,0 +1,14 @@
+import argparse
+import collections
+import random
+import sys
+from pathlib import Path
+
+import numpy as np
+import PIL
+import torch
+import torchvision
+from sconf import Config
+from prettytable import PrettyTable
+
+from domainbed.datasets import get_dataset
